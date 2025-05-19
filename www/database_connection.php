e6e084e75d6559fc63a0d1bb4be745f7d069b15d
nate.posthumus <80158058+nateposthumus@users.noreply.github.com>
2025-05-19 09:16:53 +0200

    Delete www directory

--- www/database_connection.php
+++
@@ -1,4 +0,0 @@
-<?php
-
-$conn = mysqli_connect("mariadb", "root", "password", "RO_Motoren_db");
-mysqli_set_charset($conn, "utf8mb4");
--- www/nav.php
+++
@@ -1,9 +0,0 @@
-<nav>
-        <img src="images/logo.png" alt="">
-        
-        <ul>
-            <a href="signin.php"><li class="Sign_in">Sign In</li></a>
-            <li class="tt">|</li>
-            <a href="signup.php"><li class="Sign_up">Sign Up</li></a>
-        </ul>
-    </nav>
